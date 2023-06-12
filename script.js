baf30a7eaf5fd27a3f4eb2dd66b87e5604ab009a
Ritesh Thakare <riteshthakare1191@gmail.com>
2023-06-12 22:40:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+// toggle icon navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+//  Scroll sections active link
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  // Sticky Navbar
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  // remove toggle icon and navbar when user clicks navbar link
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+// scroll reveal
+
+ScrollReveal({
+  // reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content,.heading", { origin: "top" });
+
+ScrollReveal().reveal(
+  ".home-img,.services-container,.portfolio-box,.contact-form",
+  { origin: "bottom" }
+);
+
+ScrollReveal().reveal(".home-content h1,.testimonial-wrapper", {
+  origin: "left",
+});
+
+ScrollReveal().reveal(".home-content p,.about-content, .container ul li", {
+  origin: "right",
+});
+
+ScrollReveal().reveal(".header", { origin: "top" });
+
+// typed js
+
+const typed = new Typed(".multiple-text", {
+  strings: ["Tech Enthusiast", "Coder", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+// Change copyright date automatically
+
+const year = new Date().getFullYear();
+const yearSec = document.querySelector(".year");
+yearSec.textContent = year;
